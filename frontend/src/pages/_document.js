0ae304a240f1cd438fed3a23507b5bf820ce9e5e
usmanGhani16 <afro123737@gmail.com>
2021-10-14 01:29:19 +0500

    img server side css rendring

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,9 +1,34 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    // serverside css injecting
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        // sheets.collectStyles collects all of the styles from the app’s components.
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      // sheets.getStyleElement() generates the style tag and you need to return it as props called styles
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -3,25 +3,27 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './theme';
 
 const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
-
-    *,*::after ,*::before {
+    *,
+    *::after,
+    *::before {
         padding: 0;
         margin: 0;
         box-sizing: inherit;
       }
 
-    html ,body {
+    html,
+    body {
         background: ${({ theme }) => theme.body};
         color: ${({ theme }) => theme.text};
         font-family: 'Roboto', sans-serif;
         transition: all 0.50s linear;
         font-size: 62.5%;
         box-sizing: border-box;
+        scroll-behavior: smooth;
       }
 `;
 
-export default function (props) {
+export default function Global(props) {
   return (
     <ThemeProvider theme={props.dark ? darkTheme : lightTheme}>
       <GlobalStyle />
